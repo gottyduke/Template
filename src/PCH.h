7fb0802e09dc7be71822e3ff69b267db8dbde7ac
DK <gottyduke@hotmail.com>
2022-11-16 04:58:39 -0600

    Remove direct `Windows.h` inclusion

--- src/PCH.h
+++ src/PCH.h
@@ -102,18 +102,13 @@
 #include <SKSE/SKSE.h>
 #include <REL/Relocation.h>
 
-// winnt
-#include <ShlObj_core.h>
-#include <Windows.h>
-#include <Psapi.h>
-
 using namespace std::literals;
 using namespace REL::literals;
-
-#define DLLEXPORT extern "C" [[maybe_unused]] __declspec(dllexport)
 
 // Plugin
 #include "Plugin.h"
 
 // DKUtil
 #include "DKUtil/Logger.hpp"
+
+#define DLLEXPORT extern "C" [[maybe_unused]] __declspec(dllexport)
